df049b8f8c26079ffdd4146aa8bdd1dd69ee6bb9
marcos-termac <132481294+marcos-termac@users.noreply.github.com>
2023-10-18 17:07:37 -0300

    initial commit

---
+++ my-app/src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+export function Header(){
+    return(
+        <div className="flex justify-between">
+            <div className="p-3">
+                Vpratas
+            </div>
+            <div className="p-3">
+                <ul className="flex gap-4">
+                    <li>Conjuntos</li>
+                    <li>Produtos</li>
+                    <li>Sobre nós</li>
+                    <li>Contato</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
