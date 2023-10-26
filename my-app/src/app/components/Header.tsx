a7b219416222a9b28a33a6b66b81c3358d19533b
termac1 <132481294+termac1@users.noreply.github.com>
2023-10-26 00:01:08 -0300

    initial commit

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -1,15 +1,29 @@
-export function Header(){
-    return(
-        <div className="flex justify-between">
-            <div className="p-3">
-                Vpratas
+import Image from "next/image";
+import logoImage from "../V (1).svg"
+import { UserCircle } from "@phosphor-icons/react/dist/ssr/UserCircle";
+export function Header() {
+    return (
+        <div className=" grid grid-cols-3 bg-[#003e92]">
+            <div className="flex pt-2 ml-14">
+                <Image src={logoImage} alt="logo pratas 925" width={70} />
             </div>
-            <div className="p-3">
-                <ul className="flex gap-4">
+            <div className="flex justify-center items-center">
+                <ul className="flex gap-8">
+                    <li>Correntes</li>
+                    <li>Pulseiras</li>
+                    <li>Pingentes</li>
                     <li>Conjuntos</li>
-                    <li>Produtos</li>
-                    <li>Sobre nós</li>
+                    <li>Promoções</li>
                     <li>Contato</li>
+                </ul>
+            </div>
+            <div className="items-center mr-14 flex justify-end">
+                <ul className="flex gap-7 items-center">
+                    <li>
+                        <input className="flex" type="search" />
+                    </li>
+                    <li><UserCircle size={36} /></li>
+                    <li>Carrineo</li>
                 </ul>
             </div>
         </div>
